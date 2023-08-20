cba43da2e023de317ee88514dc43701f5e87de64
FayozbekOlimov <fayozbekdeveloper@gmail.com>
2023-08-20 11:48:43 +0500

    base url changed

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://chat-app-task6.onrender.com/";
+export const BASE_URL = "https://chat-app-task6-api.onrender.com";
 export const MESSAGES_URL = "/messages";
 export const TAGS_URL = "/tags";
